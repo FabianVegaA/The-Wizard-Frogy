373f7e207893d42c54c1f38074a5ddbfd9644c65
Fabián <fabianveal@gmail.com>
2023-07-13 22:38:37 -0400

    test(list): add index and at tests

--- tests/Lib/ListTest.elm
+++ tests/Lib/ListTest.elm
@@ -1,4 +1,4 @@
-module Lib.ListTest exposing (splitAtTest, rotateTest)
+module Lib.ListTest exposing (rotateTest, splitAtTest)
 
 import Expect exposing (..)
 import Lib.List exposing (..)
@@ -54,4 +54,60 @@ rotateTest =
             \_ ->
                 rotate -1 [ 1, 2, 3 ]
                     |> Expect.equal [ 3, 1, 2 ]
+        ]
+
+
+indexTest : Test
+indexTest =
+    describe "get index of any value in a list"
+        [ test "index of 1" <|
+            \_ ->
+                index 1 [ 1, 2, 3 ]
+                    |> Expect.equal (Just 0)
+        , test "index of 2" <|
+            \_ ->
+                index 2 [ 1, 2, 3 ]
+                    |> Expect.equal (Just 1)
+        , test "index of 3" <|
+            \_ ->
+                index 3 [ 1, 2, 3 ]
+                    |> Expect.equal (Just 2)
+        , test "index of 4" <|
+            \_ ->
+                index 4 [ 1, 2, 3 ]
+                    |> Expect.equal Nothing
+        , test "index of 0" <|
+            \_ ->
+                index 0 [ 1, 2, 3 ]
+                    |> Expect.equal Nothing
+        , test "index of -1" <|
+            \_ ->
+                index -1 [ 1, 2, 3 ]
+                    |> Expect.equal Nothing
+        ]
+
+
+atTest : Test
+atTest =
+    describe "get value at index"
+        [ test "at 0" <|
+            \_ ->
+                at 0 [ 1, 2, 3 ]
+                    |> Expect.equal (Just 1)
+        , test "at an index" <|
+            \_ ->
+                at 2 [ 1, 2, 3 ]
+                    |> Expect.equal (Just 3)
+        , test "at the end" <|
+            \_ ->
+                at 3 [ 1, 2, 3 ]
+                    |> Expect.equal Nothing
+        , test "at an index larger than the list" <|
+            \_ ->
+                at 4 [ 1, 2, 3 ]
+                    |> Expect.equal Nothing
+        , test "at a negative index" <|
+            \_ ->
+                at -1 [ 1, 2, 3 ]
+                    |> Expect.equal Nothing
         ]
